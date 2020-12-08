1cad56f53d9b83035cb4547d7cc3793decfe76db
Luz Vazquez Galindo <luzvazgal@MacBook-Air-de-Luz.local>
2020-12-07 22:16:31 -0600

    home route added

--- app.py
+++ app.py
@@ -15,13 +15,17 @@ db = client.mars_db
 @app.route("/")
 def main():
     #Getting data from MongoDB
-    mars_info = list(db.mars.find())
+    mars_info = db.mars.find()
     print(mars_info)
 
-    #Render data in index.htlm
-    return render_template("index.html", teams=teams)
-    return render_template("index.html", text=my_Text, 
-    hobbies_list=hobbies, dictionary=player_dictionary)
+    title=mars_info['Title']
+    description= mars_info['Description']
+    featured_url = mars_info['Featured_Image_URL']
+    general_data= mars_info['General_Data']
+    hemisphere_imgs = mars_info['Hemisphere_Images_URLs']
+    
+    return render_template("index.html", title=title, description=description, featured_image=featured_url, 
+    general_data=general_data, hemisphere_images=hemisphere_imgs)
 
 @app.route("/scrape")
 def scrape():
--- scrape_mars.py
+++ scrape_mars.py
@@ -120,7 +120,7 @@ def scrape():
         "Title":nasa_news_title,
         "Description":nasa_news_p,
         "Featured_Image_URL": featured_image_url,
-        "General Data": tables_df[0],
+        "General_Data": tables_df[0],
         "Hemisphere_Images_URLs": hemisphere_image_urls
     }
 
